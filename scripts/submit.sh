9351b5bf34cd561aa07ade9d54bbac0d5b957ec9
Zhaomou Song <zs323@cam.ac.uk>
2020-10-12 12:35:07 +0100

    changed qsub details in submit.sh

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,3 +1,3 @@
-qsub -M qd212@cam.ac.uk -m bea -S /bin/bash -l queue_priority=cuda-low,tests=0,mem_grab=0M,gpuclass=*,osrel=*,hostname=air208 ./run_taco_wrnn.sh
+qsub -M zs323@cam.ac.uk -m bea -S /bin/bash -l queue_priority=low,tests=0,mem_grab=0M,gpuclass=*,osrel=* ./run_taco_wrnn.sh
 
 # qsub -M qd212@cam.ac.uk -m bea -S /bin/bash -l queue_priority=cuda-low,tests=0,mem_grab=0M,gpuclass=*,osrel=* ./run-aaf-tf.sh
